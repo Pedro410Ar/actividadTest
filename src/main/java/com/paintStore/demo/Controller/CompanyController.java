0da1429f3716e2171c4354852fe8740e64ed1e7b
pedro <paarruvito@hotmail.com>
2024-04-07 01:20:17 -0300

    subo company

---
+++ src/main/java/com/paintStore/demo/Controller/CompanyController.java
@@ -0,0 +1,15 @@
+package com.paintStore.demo.Controller;
+
+import com.paintStore.demo.Model.Company;
+import com.paintStore.demo.Model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class CompanyController {
+
+   /* @GetMapping(" ")
+    public List<Company> readCompany() {
+        return companyService.readFindAll();
+    }*/
+}
